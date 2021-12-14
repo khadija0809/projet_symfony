17ba67575c0f5833c06bb353159901066c6a4b84
khadija bahi <khadiija1bahi@gmail.com>
2021-12-14 16:58:29 +0100

    crud

---
+++ src/Form/ProduitType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Produit;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProduitType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom_produit')
+            ->add('description_produit')
+            ->add('prix_unite')
+            ->add('composants')
+            ->add('image_1')
+            ->add('image_2')
+            ->add('delai')
+            ->add('qte_minimum')
+            ->add('qte_maximum')
+            ->add('categorie')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Produit::class,
+        ]);
+    }
+}
